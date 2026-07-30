3208bf8a4f27d73b66a4f22eb717c9d3968aeb95
K7. <kyawzayarthant54@gmail.com>
2026-07-30 16:06:23 +0630

    fix Validation error

--- Controllers/RoleController.cs
+++ Controllers/RoleController.cs
@@ -13,7 +13,6 @@ namespace CKM_ManagementSystem.Controllers
             _roleService = roleService;
         }
 
-        
         [HttpGet]
         public IActionResult RoleEntry()
         {
@@ -21,24 +20,20 @@ namespace CKM_ManagementSystem.Controllers
             {
                 MenuPermissions = new List<MenuPermissionViewModel>
                 {
-                    
                     new MenuPermissionViewModel { MenuId = 1, MenuName = "Main Menu 1", ParentId = null },
                     new MenuPermissionViewModel { MenuId = 2, MenuName = "Submenu 1", ParentId = 1 },
                     new MenuPermissionViewModel { MenuId = 3, MenuName = "Submenu 2", ParentId = 1 },
                     new MenuPermissionViewModel { MenuId = 4, MenuName = "Submenu 3", ParentId = 1 },
 
-                   
                     new MenuPermissionViewModel { MenuId = 5, MenuName = "Main Menu 2", ParentId = null },
                     new MenuPermissionViewModel { MenuId = 6, MenuName = "Submenu 1", ParentId = 5 },
                     new MenuPermissionViewModel { MenuId = 7, MenuName = "Submenu 2", ParentId = 5 },
                     new MenuPermissionViewModel { MenuId = 8, MenuName = "Submenu 3", ParentId = 5 },
 
-                    
                     new MenuPermissionViewModel { MenuId = 9, MenuName = "Main Menu 3", ParentId = null },
                     new MenuPermissionViewModel { MenuId = 10, MenuName = "Submenu 1", ParentId = 9 },
                     new MenuPermissionViewModel { MenuId = 11, MenuName = "Submenu 2", ParentId = 9 },
 
-                    
                     new MenuPermissionViewModel { MenuId = 12, MenuName = "Main Menu 4", ParentId = null }
                 }
             };
@@ -46,7 +41,6 @@ namespace CKM_ManagementSystem.Controllers
             return View(model);
         }
 
-        
         [HttpPost]
         public async Task<IActionResult> CheckDuplicateCode(string roleCode)
         {
@@ -54,20 +48,30 @@ namespace CKM_ManagementSystem.Controllers
             return Json(new { isDuplicate = isDuplicate });
         }
 
-        
         [HttpPost]
         public async Task<IActionResult> SaveRole(RoleEntryViewModel model)
         {
+            
             if (!ModelState.IsValid)
             {
+                return View("RoleEntry", model);
+            }
+
+            
+            bool isDuplicate = await _roleService.CheckDuplicateRoleCodeAsync(model.RoleCode);
+            if (isDuplicate)
+            {
+                ModelState.AddModelError("RoleCode", "ဒီ Role Code မှာ ရှိပြီးသား ဖြစ်နေပါသည်။");
                 return View("RoleEntry", model);
             }
 
             try
             {
                 await _roleService.SaveRoleWithPermissionsAsync(model);
-                TempData["SuccessMessage"] = "Role ကို အောင်မြင်စွာ သိမ်းဆည်းပြီးပါပြီ။";
-                return RedirectToAction("RoleEntry");
+
+                
+                TempData["SuccessMessage"] = "Role သစ်ကို အောင်မြင်စွာ သိမ်းဆည်းပြီးပါပြီ။";
+                return RedirectToAction("RoleList"); 
             }
             catch (Exception ex)
             {
