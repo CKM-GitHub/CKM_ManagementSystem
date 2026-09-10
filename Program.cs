78b829e2da2687ba30172ee94f42a9a2e45b0b0b
KaungMyat1312 <kaungmyatkyn47@gmail.com>
2026-09-10 09:02:27 +0630

    Remove unused department service files

--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 using CKM_ManagementSystem.BL;
 using CKM_ManagementSystem.Data;
 using CKM_ManagementSystem.DL;
-using CKM_ManagementSystem.Services;
-using CKM_ManagementSystem.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +12,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // 2. IDepartmentService 
-builder.Services.AddScoped<IDepartmentService, DepartmentService>();
 builder.Services.AddScoped<BaseDL>();
 builder.Services.AddScoped<DepartmentBL>();
 builder.Services.AddScoped<Menu_BL>();
