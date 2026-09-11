4f80da91183a867bcdfac4e7a877ff28c7978390
Bee-bam <kaunglint530@gmail.com>
2026-09-11 13:09:33 +0630

    Refactor Program.cs for clarity and service updates
    Refactor Program.cs to clean up using directives and service registrations. Adjust exception handling and add user-related services.

--- Program.cs
+++ Program.cs
@@ -1,34 +1,29 @@
 using CKM_ManagementSystem.BL;
-<<<<<<< HEAD
 using CKM_ManagementSystem.Data;
+using CKM_ManagementSystem.DL;
 using CKM_ManagementSystem.MenuBL;
-using CKM_ManagementSystem.DL;
 using Microsoft.EntityFrameworkCore;
-using System.Runtime.InteropServices;
-=======
-using CKM_ManagementSystem.DL;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
->>>>>>> 5-rolelist
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-
-<<<<<<< HEAD
 builder.Services.AddSession();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Business Layer & Data Layer
 builder.Services.AddScoped<BaseDL>();
 builder.Services.AddScoped<DepartmentBL>();
 builder.Services.AddScoped<Menu_BL>();
 builder.Services.AddScoped<MainMenuBL>();
 builder.Services.AddScoped<RoleBL>();
-// builder.Services.AddScoped<IMenuService, MenuService>();
 builder.Services.AddScoped<LoginUserBL>();
+builder.Services.AddScoped<UserEntryBL>();
+builder.Services.AddScoped<UserListBL>();
+builder.Services.AddScoped<PasswordService>();
+
+// Authentication
 builder.Services.AddAuthentication("MyCookieAuth")
     .AddCookie("MyCookieAuth", options =>
     {
@@ -37,27 +32,12 @@ builder.Services.AddAuthentication("MyCookieAuth")
         options.ExpireTimeSpan = TimeSpan.FromHours(8);
     });
 
-
-builder.Services.AddScoped<BaseDL>();
-builder.Services.AddScoped<UserEntryBL>();
-builder.Services.AddScoped<UserListBL>();
-builder.Services.AddScoped<PasswordService>();
-=======
-
-builder.Services.AddScoped<BaseDL>();
-builder.Services.AddScoped<RoleBL>();
-
->>>>>>> 5-rolelist
 var app = builder.Build();
 
-
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
-<<<<<<< HEAD
-    app.UseExceptionHandler("Error/500");
-=======
-    app.UseExceptionHandler("/Home/Error");
->>>>>>> 5-rolelist
+    app.UseExceptionHandler("/Error/500");
     app.UseHsts();
 }
 
@@ -67,19 +47,16 @@ app.UseStatusCodePages(async context =>
     {
         context.HttpContext.Response.Redirect("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtu8SLcS1IQcINzO_ilRCY1APLalIhxU5Oi3eU8YUncg&s=10");
     }
-
     await Task.CompletedTask;
 });
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseSession();
 app.UseAuthorization();
-
 
 app.MapControllerRoute(
     name: "default",
