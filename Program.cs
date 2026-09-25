d5d4af643f27e9208f47760d43349d7421c6826c
BuuThee <berdap19@gmail.com>
2026-09-25 16:18:14 +0630

    Updating Permission features

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using CKM_ManagementSystem.Authorization;
-using CKM_ManagementSystem.Permissions;
 using CKM_ManagementSystem.BL;
 using CKM_ManagementSystem.Data;
-using CKM_ManagementSystem.MenuBL;
 using CKM_ManagementSystem.DL;
+using CKM_ManagementSystem.MenuBL;
+using CKM_ManagementSystem.Permissions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 
@@ -41,16 +43,45 @@ builder.Services.AddScoped<CurrentUserPermission>();
 builder.Services.AddScoped<IAuthorizationHandler, PermissionAuthorizationHandler>();
 builder.Services.AddSingleton<IAuthorizationPolicyProvider, PermissionPolicyProvider>();
 
-
+// ---------- Authentication ----------
 builder.Services.AddAuthentication("MyCookieAuth")
     .AddCookie("MyCookieAuth", options =>
     {
         options.Cookie.Name = "CKM_AuthCookie";
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+
         options.LoginPath = "/LoginUsers/Login";
-        options.AccessDeniedPath = "/Error/StatusCode/403";   
+        options.AccessDeniedPath = "/Error/StatusCode/403";
         options.ExpireTimeSpan = TimeSpan.FromHours(8);
         options.SlidingExpiration = true;
 
+        options.Events = new CookieAuthenticationEvents
+        {
+            OnRedirectToLogin = ctx =>
+            {
+                if (ctx.Request.Path.StartsWithSegments("/LoginUsers") ||
+                    ctx.Request.Path.StartsWithSegments("/Error"))
+                {
+                    ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    return Task.CompletedTask;
+                }
+                ctx.Response.Redirect(ctx.RedirectUri);
+                return Task.CompletedTask;
+            },
+            OnRedirectToAccessDenied = ctx =>
+            {
+                if (ctx.Request.Path.StartsWithSegments("/LoginUsers") ||
+                    ctx.Request.Path.StartsWithSegments("/Error"))
+                {
+                    ctx.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    return Task.CompletedTask;
+                }
+                ctx.Response.Redirect(ctx.RedirectUri);
+                return Task.CompletedTask;
+            }
+        };
     });
 
 builder.Services.AddAuthorization(options =>
@@ -66,16 +97,15 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseStatusCodePagesWithReExecute("/Error/StatusCode/{0}");
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
-app.UseSession();         
+app.UseSession();
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseStatusCodePagesWithReExecute("/Error/StatusCode/{0}");
 
 app.MapControllerRoute(
     name: "default",
